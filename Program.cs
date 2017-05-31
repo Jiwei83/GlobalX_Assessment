a75a7f035cf75d73484ded47269960d1e030fd0d
Jiwei <kricma83@gmail.com>
2017-05-31 23:49:30 +1000

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -7,15 +7,16 @@ namespace Solution
     {
         public static void Main(string[] args)
         {
+	    string pathForReading = "";
             ReadFile rf = new ReadFile();
-            rf.GetFile(@"/Users/majiwei/Desktop/test");
+            rf.GetFile(pathForReading, pathForWriting);
         }
     }
 
     public class ReadFile 
     {
         //Read the content of file and display
-		public void GetFile(string filename)
+		public void GetFile(string filename, string pathForWriting)
 		{
             //Read the file
 			string[] lines = System.IO.File.ReadAllLines(filename);
@@ -27,7 +28,7 @@ namespace Solution
 			{
                 Console.WriteLine(line);
 			}
-            notExisted = WriteFile(@"/Users/majiwei/Desktop/names-sorted.txt", lines);
+            notExisted = WriteFile(pathForWriting, lines);
             //Check the file is existed or not
             if (notExisted) {
 				Console.WriteLine("Finished: created names - sorted.txt");
